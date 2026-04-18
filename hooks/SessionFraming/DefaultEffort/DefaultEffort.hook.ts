a94e186563a1ff47bf0b200c1ee75fee5483cd62
Ian Hogers <ianhogers@hotmail.com>
2026-04-18 16:18:01 +1000

    revert(DefaultEffort): remove hook in favor of CLAUDE_CODE_EFFORT_LEVEL env var
    The env var is simpler and actually sets the effort level, unlike the hook
    which only injected a text instruction.
    
    Added to ~/.zshrc: export CLAUDE_CODE_EFFORT_LEVEL="max"
    
    Co-Authored-By: Maple <ianhogers@hotmail.com>

--- hooks/SessionFraming/DefaultEffort/DefaultEffort.hook.ts
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bun
-import { runHook } from "@hooks/core/runner";
-import { DefaultEffort } from "@hooks/hooks/SessionFraming/DefaultEffort/DefaultEffort.contract";
-
-runHook(DefaultEffort);
